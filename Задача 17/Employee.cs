5ffc2a29d0b788df1ea2aa860c69ef8561e60638
homew <homew@DESKTOP-NULQPNB>
2023-02-25 12:43:57 +0300

    Добавьте файлы проекта.

---
+++ Задача 17/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_17
+{
+    internal class Employee<T>
+    {
+        public T Num { get; set; }
+        
+        public int Summa { get; set; }
+        public string Name { get; set; }
+        public string GetInfo()
+        {
+            return $"{Num} {Summa} {Name}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+            Console.WriteLine("Введите баланс счета");
+            Summa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца");
+            Name = Console.ReadLine();
+        }
+    }
+}
---
+++ Задача 17/Program.cs
@@ -0,0 +1,9 @@
+﻿using Задача_17;
+
+Employee<int> employee1 = new Employee<int>();
+employee1.Input();
+Console.WriteLine(employee1.GetInfo());
+
+Employee<string> employee2 = new Employee<string>();
+employee2.Input();
+Console.WriteLine(employee2.GetInfo());
